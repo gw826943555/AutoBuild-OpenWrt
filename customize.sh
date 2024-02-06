237911ed6167b653d599e1f6eb10e77f3e0545b4
william <gw826943555@qq.com>
2024-02-07 01:06:10 +0800

    initial for rax3000m

--- customize.sh
+++ customize.sh
@@ -5,11 +5,17 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+# 修改默认时区
+sed -i "s/timezone='UTC'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
+# 修改默认地区
+sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+# 开启TCP Fast Open
+#sed -i '/memberships/a\\\tnet.ipv4.tcp_fastopen=3' ./package/base-files/files/etc/sysctl.d/10-default.conf
+# 修改最大连接数
+#sed -i 's/nf_conntrack_max=16384/nf_conntrack_max=65536/g' ./package/kernel/linux/files/sysctl-nf-conntrack.conf
+# 开启WiFi
+sed -i 's/radio${devidx}.disabled=1/radio${devidx}.disabled=0/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 开启software flow offloading
+sed -i '/^config defaults/a \\toption flow_offloading\t1' ./package/network/config/firewall/files/firewall.config
+# 修改默认IP地址
+sed -i 's/192.168.1.1/192.168.111.1/g' ./package/base-files/files/bin/config_generate
