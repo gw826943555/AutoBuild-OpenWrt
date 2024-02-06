ff7c6566e4c971b270caf7855a1b7fdb429dbff5
william <gw826943555@qq.com>
2024-02-07 01:21:10 +0800

    fix customize

--- customize.sh
+++ customize.sh
@@ -5,6 +5,7 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
+cd openwrt
 # 修改默认时区
 sed -i "s/timezone='UTC'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 # 修改默认地区
@@ -14,7 +15,7 @@ sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia/Shanghai'" ./pack
 # 修改最大连接数
 #sed -i 's/nf_conntrack_max=16384/nf_conntrack_max=65536/g' ./package/kernel/linux/files/sysctl-nf-conntrack.conf
 # 开启WiFi
-sed -i 's/radio${devidx}.disabled=1/radio${devidx}.disabled=0/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/wireless.${name}.disabled=1/wireless.${name}.disabled=0/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 开启software flow offloading
 sed -i '/^config defaults/a \\toption flow_offloading\t1' ./package/network/config/firewall/files/firewall.config
 # 修改默认IP地址
