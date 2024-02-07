0c5e78933db4b1083525436dbefa6cdd68a730cb
gw826943555 <gw826943555@qq.com>
2024-02-07 13:04:52 +0800

    fix radio customize

--- customize.sh
+++ customize.sh
@@ -15,7 +15,8 @@ sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia/Shanghai'" ./pack
 # 修改最大连接数
 #sed -i 's/nf_conntrack_max=16384/nf_conntrack_max=65536/g' ./package/kernel/linux/files/sysctl-nf-conntrack.conf
 # 开启WiFi
-sed -i 's/wireless.${name}.disabled=1/wireless.${name}.disabled=0/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/wireless.${name}.disabled=1/wireless.${name}.disabled=0/g' ./package/network/config/wifi-scripts/files/lib/wifi
+/mac80211.sh
 # 开启software flow offloading
 sed -i '/^config defaults/a \\toption flow_offloading\t1' ./package/network/config/firewall/files/firewall.config
 # 修改默认IP地址
